3399c5bbc0be11755a185fab75d5cb2654cf6db6
Axel Jacobsen <aejacobs@stud.ntnu.no>
2022-11-21 13:09:51 +0100

    Patch permission loop

--- app/src/main/java/com/example/myapplication/CameraButtonFragment.kt
+++ app/src/main/java/com/example/myapplication/CameraButtonFragment.kt
@@ -7,17 +7,21 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.provider.MediaStore
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import java.lang.Thread.sleep
 
 /**
  * Opens a dialog asking the user if they wish to take a picture or load from gallery
  */
 class CameraButtonFragment : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?) : Dialog {
-        print("test")
         return activity?.let{
             val builder = AlertDialog.Builder(it)
             builder.setMessage("New Note")
@@ -31,29 +35,38 @@ class CameraButtonFragment : DialogFragment() {
         } ?: throw IllegalStateException("Activity cannot be null")
     }
 
-    //requests permission, then opens camera
+    /**
+     * Requests permission for camera use and opens the camera
+     */
     private fun takeAPic(){
-        val cameraPermits = ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.CAMERA)
-        if (cameraPermits != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(requireActivity(),
-            arrayOf(android.Manifest.permission.CAMERA), cameraPermits)
-        } else {
-            val takePicIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            getResult.launch(takePicIntent);
-        }
-    }
-    //Requests permission, then opens gallery
-    private fun uploadFromGallery(){
-        val libraryPermit =ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.READ_EXTERNAL_STORAGE)
-        if (libraryPermit != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(requireActivity(),arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), libraryPermit)
-        } else {
-            val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-            getResult.launch(galleryIntent);
+        createPermissionRequest(android.Manifest.permission.CAMERA, 1){ gotPermission ->
+            if (gotPermission){
+                val takePicIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                getResult.launch(takePicIntent)
+            } else {
+                Toast.makeText(requireContext(), "Camera permission denied, update in device settings", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
-    // Handles intent
+    /**
+     * Requests permission for gallery use and opens the gallery
+     */
+    private fun uploadFromGallery(){
+        createPermissionRequest(android.Manifest.permission.READ_EXTERNAL_STORAGE, 2) { gotPermission ->
+            if (gotPermission) {
+                val galleryIntent =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                getResult.launch(galleryIntent)
+            } else {
+                Toast.makeText(requireContext(), "Gallery permission denied, update in device settings", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    /**
+     * Handles launching intent
+     */
     private val getResult =
         registerForActivityResult(
             ActivityResultContracts.StartActivityForResult()) {
@@ -61,4 +74,19 @@ class CameraButtonFragment : DialogFragment() {
                 val value = it.data?.getStringExtra("input")
             }
         }
+
+    /**
+     * Creates a permissions request and returns synchronously
+     */
+    private fun createPermissionRequest(perm: String, requestCode: Int, onSuccess:(Boolean)->Unit){
+        val permit = context?.checkSelfPermission(perm)
+        if (permit != PackageManager.PERMISSION_GRANTED) {
+            activity?.requestPermissions(arrayOf(perm),requestCode)
+            if (context?.checkSelfPermission(perm) != PackageManager.PERMISSION_GRANTED){
+                onSuccess(false)
+            }
+            onSuccess(true)
+        }
+        else onSuccess(true)
+    }
 }
