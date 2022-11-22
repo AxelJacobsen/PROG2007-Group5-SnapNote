ef2c7b65c442b887d59403f82c9c9c43c67791c8
Axel Jacobsen <aejacobs@stud.ntnu.no>
2022-11-22 19:50:43 +0100

    Fix send camera & gallery image to activity_note

--- app/src/main/java/com/example/myapplication/CameraButtonFragment.kt
+++ app/src/main/java/com/example/myapplication/CameraButtonFragment.kt
@@ -10,23 +10,19 @@ import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
-import java.lang.Thread.sleep
 
 /**
  * Opens a dialog asking the user if they wish to take a picture or load from gallery
  */
 class CameraButtonFragment : DialogFragment() {
-
-    val CAMER_PERM = 1
+    val CAMERA_PERM = 1
+    val CAMERA_STRING = android.Manifest.permission.CAMERA
     val GALLERY_PERM = 2
-
+    val GALLERY_STRING = android.Manifest.permission.READ_EXTERNAL_STORAGE
+    var currentPermString = ""
     override fun onCreateDialog(savedInstanceState: Bundle?) : Dialog {
         return activity?.let{
             val builder = AlertDialog.Builder(it)
@@ -45,13 +41,14 @@ class CameraButtonFragment : DialogFragment() {
      * Requests permission for camera use and opens the camera
      */
     private fun takeAPic(){
-        createPermissionRequest(android.Manifest.permission.CAMERA, CAMER_PERM){ gotPermission ->
-            if (gotPermission){
-                val takePicIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                getResult.launch(takePicIntent)
-            } else {
-                Toast.makeText(requireContext(), "Camera permission denied, update in device settings", Toast.LENGTH_SHORT).show()
-            }
+        if (context?.checkSelfPermission(CAMERA_STRING) == PackageManager.PERMISSION_GRANTED){
+            //opens camera
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            getResult.launch(cameraIntent)
+        } else {
+            //Updates public permstring since you cant retrieve parameter in function variable
+            currentPermString = CAMERA_STRING
+            requestPermission.launch(CAMERA_STRING)
         }
     }
 
@@ -59,14 +56,14 @@ class CameraButtonFragment : DialogFragment() {
      * Requests permission for gallery use and opens the gallery
      */
     private fun uploadFromGallery(){
-        createPermissionRequest(android.Manifest.permission.READ_EXTERNAL_STORAGE, GALLERY_PERM) { gotPermission ->
-            if (gotPermission) {
-                val galleryIntent =
-                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-                getResult.launch(galleryIntent)
-            } else {
-                Toast.makeText(requireContext(), "Gallery permission denied, update in device settings", Toast.LENGTH_SHORT).show()
-            }
+        if (context?.checkSelfPermission(GALLERY_STRING) == PackageManager.PERMISSION_GRANTED){
+            //Opens gallery, due to lack of images saving this is untested
+            val galleryIntent =
+                Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            getResult.launch(galleryIntent)
+        } else {
+            currentPermString = GALLERY_STRING
+            requestPermission.launch(GALLERY_STRING)
         }
     }
 
@@ -74,41 +71,56 @@ class CameraButtonFragment : DialogFragment() {
      * Handles launching intent
      */
     private val getResult =
-        registerForActivityResult(
-            ActivityResultContracts.StartActivityForResult()) {
-            if(it.resultCode == Activity.RESULT_OK){
-                val value = it.data?.getStringExtra("input")
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            /**
+             * Greeting Mr.Teacher or whoever might be reading this. My name is Axel Jacobsen
+             * and everything past this is never called. Why you may ask? WELL, let me tell you why.
+             * Android studio has a built in function called onActivityResult, this function
+             * is deprecated in the newer version of android studio. Therefore, i wished not to use
+             * it. After days of research this was the only alternative, however, it didn't work.
+             *
+             * Now as to why this didn't work remained a mystery for a while, until desperation, and
+             * the debugger tool told me that a default onActivityResult was catching the result
+             * from the camera BEFORE this did. Why a deprecated function had priority is a mystery.
+             * Internet could not help me since this is a relatively
+             * new change. So here we are. If for whatever reason this code finally gets its turn
+             * then it should work. However i have no way of testing it since android studio is
+             * staunchly denying my every attempt to use new and updated code.
+             */
+            var outImage = ImageClass(null, null)
+            if(result.resultCode == Activity.RESULT_OK){
+                if ( result.resultCode==CAMERA_PERM){
+                    outImage.bitmap = result.data?.extras?.get("data") as Bitmap
+                }
+                else if (result.resultCode == GALLERY_PERM){
+                    outImage.uri = result.data?.data as Uri
+                }
+                val intent = Intent(requireContext(), NoteActivity::class.java)
+                intent.putExtra("picture", outImage)
+                startActivity(intent)
             }
         }
 
     /**
-     * Creates a permissions request and returns synchronously
+     * Requests permission to use camera and gallery
      */
-    private fun createPermissionRequest(perm: String, requestCode: Int, onSuccess:(Boolean)->Unit){
-        val permit = context?.checkSelfPermission(perm)
-        if (permit != PackageManager.PERMISSION_GRANTED) {
-            activity?.requestPermissions(arrayOf(perm),requestCode)
-            if (context?.checkSelfPermission(perm) != PackageManager.PERMISSION_GRANTED){
-                onSuccess(false)
+    private val requestPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            gotPermission: Boolean ->
+            if (gotPermission){
+                if (currentPermString ==CAMERA_STRING) {
+                    val takePicIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                    Toast.makeText(requireContext(), "Entered camera", Toast.LENGTH_SHORT).show()
+                    getResult.launch(takePicIntent)
+                } else if (currentPermString == GALLERY_STRING){
+                    val galleryIntent =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    getResult.launch(galleryIntent)
+                } else {
+                    Toast.makeText(requireContext(), "Illegal Access type", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(requireContext(), "Cant access camera or gallery data, update permissions in settings to access", Toast.LENGTH_SHORT).show()
             }
-            onSuccess(true)
         }
-        else onSuccess(true)
-    }
-
-    /**
-     * Creates a permissions request and returns synchronously
-
-        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        var image = ImageClass(null, null)
-        if(requestCode==CAMER_PERM && resultCode == Activity.RESULT_OK){
-            image.bitmap = data?.extras?.get("data") as Bitmap
-        }else if (requestCode == GALLERY_PERM && resultCode == Activity.RESULT_OK){
-            image.uri = data?.data as Uri
-        }
-        super.onActivityResult(requestCode, resultCode, data)
-        val intent = Intent(this, NoteActivity::class.java)
-        intent.putExtra("picture", image)
-        startActivity(intent)
-    }*/
 }
